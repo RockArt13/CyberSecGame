75b061a311fe1c04f07872fcee9357654d433808
RockArt13 <rtyomgrigoryan@gmail.com>
2022-10-09 02:02:17 +0300

    Update waypoints.cs

--- Assets/waypoints.cs
+++ Assets/waypoints.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class waypoints : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public static Transform[] points;
+
+
+    private void Awake()
     {
-        
+        points = new Transform[transform.childCount];
+
+        for(int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
