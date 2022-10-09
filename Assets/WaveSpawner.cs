ca09800701c1fecce876a5225858a64176b28338
RockArt13 <rtyomgrigoryan@gmail.com>
2022-10-09 13:31:25 +0300

    WaveSpawner update

--- Assets/WaveSpawner.cs
+++ Assets/WaveSpawner.cs
@@ -1,28 +1,41 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveSpawner : MonoBehaviour
 {
     public Transform enemyPrefab;
 
+    public Transform spawnPoint;
+
     public float timeBetweenWaves = 5f;
     private float countdown = 2f;
+
+    private int waveIndex = 0;
 
        private void Update()
     {
         if (countdown<=0f)
         {
-            SpawnWave();
+            StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;
-
         }
 
         countdown -= Time.deltaTime;
     }
 
-    void SpawnWave()
+    IEnumerator SpawnWave()
     {
-        Debug.Log("Loading the wave!");
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+       
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
     }
     }
