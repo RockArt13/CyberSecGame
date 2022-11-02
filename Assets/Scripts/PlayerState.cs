f0831adc2a5e6878dd70e85a126430a9e2475c56
RockArt13 <rtyomgrigoryan@gmail.com>
2022-11-02 02:54:31 +0200

    Revert "test"
    This reverts commit f4adad816c0e716c58559420db78cb7aed6d3444.

--- Assets/Scripts/PlayerState.cs
+++ Assets/Scripts/PlayerState.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 public class PlayerState : MonoBehaviour
 {
     public static int Money;
-    public static int Lives;
 
     public int startMoney = 500;
-    public int startLives = 10;
 
     private void Start()
     {
         Money = startMoney;
-        Lives = startLives;
     }
 
 
