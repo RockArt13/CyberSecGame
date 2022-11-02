5e504c162cc78a01b06a2129d7b7e475e1a3a471
RockArt13 <rtyomgrigoryan@gmail.com>
2022-11-02 03:29:10 +0200

    UpdatedLives

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,8 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 70f;
+
+    public int damage = 50;
     public float explosionRadius = 0f;
     public GameObject bulletEffect;
 
@@ -69,7 +71,13 @@ public class Bullet : MonoBehaviour
     }
     void Damage(Transform enemy)
     {
-        Destroy(enemy.gameObject);
+
+        Enemy e = enemy.GetComponent<Enemy>();
+
+        if (e!= null)
+        {
+            e.TakeDamage(damage);
+        }
     }
 
     private void OnDrawGizmosSelected()
