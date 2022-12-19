5602b77d523e0eb20c877b3be4c2f47dc0bf2f93
RockArt13 <rtyomgrigoryan@gmail.com>
2022-12-19 15:32:01 +0200

    HealthBar added #24

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,15 +7,21 @@ public class Enemy : MonoBehaviour
     [HideInInspector]
     public float speed;
 
-    public float health = 100;
+
+    public float startHealth = 100f;
+    private float health;
 
     public int value = 50;
 
     public GameObject deathEffect;
 
+    [Header("Unity Related")]
+    public Image healthBar;
+
     private void Start()
     {
         speed = startSpeed;
+        health = startHealth;
     }
 
 
@@ -24,6 +29,8 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(float amount)
     {
         health -= amount;
+
+        healthBar.fillAmount = health / startHealth;
 
         if(health<0)
         {
