1e695a0a2259da397777eaaa86696b8cf07bc036
RockArt13 <rtyomgrigoryan@gmail.com>
2022-12-20 14:00:37 +0200

    #31 button disabled animation added

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -1,10 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelSelect : MonoBehaviour
 {
     public SceneFade fade;
+
+    public Button[] levelButtons;
+
+    private void Start()
+    {
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            levelButtons[i].interactable = false;
+        }
+    }
 
     public void Select(string levelName)
     {
